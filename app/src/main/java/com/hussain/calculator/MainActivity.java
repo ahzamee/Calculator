c50ce151a764cb11f8b84211e8d8727123166e78
zamee <hussain01673@gmail.com>
2018-07-25 03:34:49 +0600

    initial commit

---
+++ app/src/main/java/com/hussain/calculator/MainActivity.java
@@ -0,0 +1,33 @@
+package com.hussain.calculator;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        value = findViewById(R.id.value);
+    }
+
+    public void calculate(View view) {
+
+        String Value = value.getText().toString();
+        showMessage("Calculated Value is: ",Value);
+    }
+
+    public void showMessage(String title, String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
