b925aa7412cddba8cbc3978c451cb6be673c6164
zamee <zamee0192@gmail.com>
2022-02-18 12:30:45 +0600

    title changed

--- app/src/main/java/com/hussain/calculator/MainActivity.java
+++ app/src/main/java/com/hussain/calculator/MainActivity.java
@@ -60,52 +60,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        String infixToPostfix(String exp)
+    String infixToPostfix(String exp)
+    {
+        // initializing empty String for result
+        String result = new String("");
+
+        // initializing empty stack
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i<exp.length(); ++i)
         {
-            // initializing empty String for result
-            String result = new String("");
+            char c = exp.charAt(i);
 
-            // initializing empty stack
-            Stack<Character> stack = new Stack<>();
+            // If the scanned character is an operand, add it to output.
+            if (Character.isLetterOrDigit(c))
+                result += c;
 
-            for (int i = 0; i<exp.length(); ++i)
+                // If the scanned character is an '(', push it to the stack.
+            else if (c == '(')
+                stack.push(c);
+
+                //  If the scanned character is an ')', pop and output from the stack
+                // until an '(' is encountered.
+            else if (c == ')')
             {
-                char c = exp.charAt(i);
+                while (!stack.isEmpty() && stack.peek() != '(')
+                    result += stack.pop();
 
-                // If the scanned character is an operand, add it to output.
-                if (Character.isLetterOrDigit(c))
-                    result += c;
-
-                    // If the scanned character is an '(', push it to the stack.
-                else if (c == '(')
-                    stack.push(c);
-
-                    //  If the scanned character is an ')', pop and output from the stack
-                    // until an '(' is encountered.
-                else if (c == ')')
-                {
-                    while (!stack.isEmpty() && stack.peek() != '(')
-                        result += stack.pop();
-
-                    if (!stack.isEmpty() && stack.peek() != '(')
-                        return "Invalid Expression"; // invalid expression
-                    else
-                        stack.pop();
-                }
-                else // an operator is encountered
-                {
-                    while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()))
-                        result += stack.pop();
-                    stack.push(c);
-                }
+                if (!stack.isEmpty() && stack.peek() != '(')
+                    return "Invalid Expression"; // invalid expression
+                else
+                    stack.pop();
             }
-
-            // pop all the operators from the stack
-            while (!stack.isEmpty())
-                result += stack.pop();
-
-            return result;
+            else // an operator is encountered
+            {
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()))
+                    result += stack.pop();
+                stack.push(c);
+            }
         }
+
+        // pop all the operators from the stack
+        while (!stack.isEmpty())
+            result += stack.pop();
+
+        return result;
+    }
 
     static double evaluatePostfix(String exp)
     {
